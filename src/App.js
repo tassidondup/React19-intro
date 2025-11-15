e65f0e6f3e93dc10187ee5e65c645341b27212a0
tashi dhondup <tashidhondup@tashis-MacBook-Pro.local>
2025-11-16 10:41:09 +1100

    Created Pizza component that accepts name and description as props

---
+++ src/App.js
@@ -0,0 +1,32 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Hello from React 19"),
+    React.createElement(Pizza, {
+      name: "Pepperoni Pizza",
+      description: "Mozzarella cheese, Pepperoni, Tomato sauce",
+    }),
+    React.createElement(Pizza, {
+      name: "Margherita Pizza",
+      description: "Fresh mozzarella, Basil, Tomato sauce",
+    }),
+    React.createElement(Pizza, {
+      name: "BBQ Chicken Pizza",
+      description: "Grilled chicken, BBQ sauce, Red onions",
+    }),
+    React.createElement(Pizza, {
+      name: "Veggie Pizza",
+      description: "Bell peppers, Olives, Onions, Mushrooms",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
