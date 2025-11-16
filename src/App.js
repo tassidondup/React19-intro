1af423e3d172693cd1494a3a192d810c6fe0898a
tashi dhondup <tashidhondup@tashis-MacBook-Pro.local>
2025-11-16 13:05:44 +1100

    Created separate Pizza component and used jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
-const Pizza = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("p", {}, props.description),
-  ]);
-};
+import Pizza from "./Pizza";
 
 const App = () => {
   return React.createElement("div", {}, [
---
+++ src/Pizza.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+
+const Pizza = (props) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>{props.description}</p>
+    </div>
+  );
+};
+
+export default Pizza;
